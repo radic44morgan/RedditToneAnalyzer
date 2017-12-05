bf762d4f049bff31cf2606ef97c3f3e90e6d7ceb
Morgan Radic <radic44morgan@gmail.com>
2017-12-05 14:44:09 -0500

    finished demo

--- scripts/new-age.js
+++ scripts/new-age.js
@@ -1,14 +1,15 @@
 (function($) {
   "use strict"; // Start of use strict
-  
-  var snoowrap = require('snoowrap');
-  const r = new snoowrap({
-      userAgent: 'morganradic',
-      clientId: 'A_EtHItRrL0i6A',
-      clientSecret: 'RwXvHAR_beIZR81hzhS1z0gQwIQ',
-      refreshToken: '42316489-W0ozardOWQf2mexRSOW2opleL0Q'
-    });
-  r.getSubreddit('Showerthoughts').getHot().map(post => post.title).then(console.log)
+
+  //use this stuff l8r 
+  // var snoowrap = require('snoowrap');
+  // const r = new snoowrap({
+  //     userAgent: 'morganradic',
+  //     clientId: 'A_EtHItRrL0i6A',
+  //     clientSecret: 'RwXvHAR_beIZR81hzhS1z0gQwIQ',
+  //     refreshToken: '42316489-W0ozardOWQf2mexRSOW2opleL0Q'
+  //   });
+  // r.getSubreddit('Showerthoughts').getHot().map(post => post.title).then(console.log)
   
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
@@ -35,6 +36,7 @@
     offset: 54
   });
 
+
   // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
@@ -48,4 +50,68 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-})(jQuery); // End of use strict
+})
+
+function getPosts()
+{
+  document.getElementById("preAnalyze").style.display = "none";
+  document.getElementById("postAnalyze").style.display = "block";
+  document.getElementById("postSub").innerHTML = "r/" + document.getElementById("getSub").value + ": ";
+  document.getElementById("postTone").innerHTML = getTone() + ": " + getPercentage() + "%";
+  
+}
+
+function newSub()
+{
+  document.getElementById("preAnalyze").style.display = "block";
+  document.getElementById("postAnalyze").style.display = "none";
+}
+
+function getTone()
+{
+  var rand = Math.floor(Math.random() * 10);
+  var tone = "";
+  switch (rand) {
+    case 0:
+        tone = "Anger";
+        break;
+    case 1:
+        tone = "Disgust";
+        break;
+    case 2:
+        tone = "Fear";
+        break;
+    case 3:
+        tone = "Joy";
+        break;
+    case 4:
+        tone = "Analytical";
+        break;
+    case 5:
+        tone = "Confident";
+        break;
+    case 6:
+        tone = "Tentative";
+        break;
+    case 7:
+        tone = "Openness";
+        break;
+    case 8:
+        tone = "Conscientiousness";
+        break;
+    case 9:
+        tone = "Extraversion";
+        break;
+    case 10:
+        tone = "Agreeableness";
+        break;
+      }
+  return tone;
+}
+
+function getPercentage()
+{
+  return Math.floor(Math.random()*100);
+}
+
+(jQuery); // End of use strict
